095e3035e4d39377d3d6e78cc8de39145d010428
Mahdi Dehghani <mahdi14dehghani@gmail.com>
2025-10-08 20:36:14 +0330

    feat : write middleware

---
+++ middleware.js
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server';
+import { auth } from './auth';
+
+export async function middleware(req) {
+  const session = await auth();
+  if (
+    !session ||
+    !session?.accessToken ||
+    session?.error === 'RefreshAccessTokenError'
+  ) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/dashboard/:path*']
+};
+
